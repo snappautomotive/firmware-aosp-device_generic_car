8f4379a227ebbf6b597029f17d82cbc4c9be36d4
Nicholas Sauer <nicksauer@google.com>
2017-09-27 06:31:56 -0700

    Fix build fingerprint length of aosp_car emulator targets.
    bug: 66989354
    Test: lunch aosp_car_x86 && m -j
    Change-Id: I9049dc984908602a2e7bc8e54cac3d0632f34a41

--- aosp_car_x86.mk
+++ aosp_car_x86.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 $(call inherit-product, device/generic/car/common/car.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86_64.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86.mk)
 
-PRODUCT_NAME := aosp_car_emu_x86_64
-PRODUCT_DEVICE := generic_x86_64
+PRODUCT_NAME := aosp_car_x86
+PRODUCT_DEVICE := generic_x86
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := Car on x86_64 emulator
+PRODUCT_MODEL := Car on x86 emulator
