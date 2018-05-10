8a1a1c4d8a583ce31e4c7adfe01d14dce6fd9f94
Hongwei Wang <hwwang@google.com>
2018-05-10 10:53:35 -0700

    Simulates audio mixer
    Bug: 79496296
    Test: Play music + navigation in KitchenSink, gcar_emu_x86-userdebug
    Change-Id: I335183e2428e655d1cd81141d49326b457669383

--- emulator/audio/driver/ext_pcm.h
+++ emulator/audio/driver/ext_pcm.h
@@ -18,20 +18,34 @@
 #define EXT_PCM_H
 
 #include <pthread.h>
+
+#include <cutils/hashmap.h>
 #include <tinyalsa/asoundlib.h>
+
+// Holds up to 4KB buffer for each mixer pipeline, this value is arbitrary chosen
+#define MIXER_BUFFER_SIZE (1024 * 4)
+
+struct ext_mixer_pipeline {
+  int16_t buffer[MIXER_BUFFER_SIZE];
+  unsigned int position;
+};
 
 struct ext_pcm {
   struct pcm *pcm;
   pthread_mutex_t lock;
   unsigned int ref_count;
+  pthread_mutex_t mixer_lock;
+  struct ext_mixer_pipeline mixer_pipeline;
+  pthread_t mixer_thread;
+  Hashmap *mixer_pipeline_map;
 };
 
 struct ext_pcm *ext_pcm_open(unsigned int card, unsigned int device,
                              unsigned int flags, struct pcm_config *config);
 int ext_pcm_close(struct ext_pcm *ext_pcm);
 int ext_pcm_is_ready(struct ext_pcm *ext_pcm);
-int ext_pcm_write(struct ext_pcm *ext_pcm, const void *data,
-                  unsigned int count);
+int ext_pcm_write(struct ext_pcm *ext_pcm, const char *bus_address,
+                  const void *data, unsigned int count);
 const char *ext_pcm_get_error(struct ext_pcm *ext_pcm);
 unsigned int ext_pcm_frames_to_bytes(struct ext_pcm *ext_pcm,
                                      unsigned int frames);
