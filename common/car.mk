809f8b88914b9e907b019bd16b04215f2ee370cf
Nicholas Sauer <nicksauer@google.com>
2019-07-01 17:16:45 -0700

    Fix the car emulator.
    -Sync with the goldfish manifest.
    -Use DEVICE_MANIFEST_FILE to specify manifest.
    
    bug: 135458450
    Test: lunch gcar_hl_emu_x86-userdebug && m && emulator
    Change-Id: I18ac986ebc4b799c4a5fdc8541f4af471606d684

--- common/car.mk
+++ common/car.mk
@@ -60,8 +60,6 @@ PRODUCT_COPY_FILES += \
     device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
     device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml
 
-# Vendor Interface Manifest
-PRODUCT_COPY_FILES += \
-    device/generic/car/common/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
+DEVICE_MANIFEST_FILE := device/generic/car/common/manifest.xml
 
 $(call inherit-product, packages/services/Car/car_product/build/car.mk)
