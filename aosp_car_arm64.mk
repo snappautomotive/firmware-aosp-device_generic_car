52cf1a678cf3a6b232810e3f205b093784eaae93
Mark Grady <gradymark@google.com>
2019-10-16 02:18:07 +0000

    Revert "Add car audio emu support to aosp_car_* targets"
    This reverts commit 65f83fa730fcdbd80eaa1646800f958df96f48eb.
    
    Reason for revert: depends upon deprecated car_emulator_hal.mk
    
    Change-Id: I584c7a77d46b29d7402570206dd004d41c05ccc8

--- aosp_car_arm64.mk
+++ aosp_car_arm64.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
+$(call inherit-product, device/generic/car/common/car.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64.mk)
 
 PRODUCT_NAME := aosp_car_arm64
--- aosp_car_x86_64.mk
+++ aosp_car_x86_64.mk
@@ -13,11 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
+$(call inherit-product, device/generic/car/common/car.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86_64.mk)
 
 PRODUCT_NAME := aosp_car_x86_64
 PRODUCT_DEVICE := generic_x86_64
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Car on x86_64 emulator
-
