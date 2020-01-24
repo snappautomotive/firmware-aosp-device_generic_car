20966be438c19121651dba80c5711a5e58d98acf
Oscar Azucena <oscarazu@google.com>
2020-01-24 00:03:11 +0000

    Revert "Include core makefile before device-specific one."
    Revert "Include core makefile before device-specific one."
    
    Revert submission 10117438-car-mk-order
    
    Reason for revert: The auto emulator looks like a giant phone.
    Bug: 148219586
    Reverted Changes:
    Iafb4d62c1: Include core makefile before device-specific one.
    I1663477b0: Include core makefile before device-specific one.
    Test: make -j60 and ran emulator to make sure the auto audio properties
    were loaded correctly.
    Change-Id: I8d53b85dd6def993e4886d8859e4527548b184c7

--- aosp_car_arm64.mk
+++ aosp_car_arm64.mk
@@ -15,8 +15,8 @@
 
 PRODUCT_PACKAGE_OVERLAYS := device/generic/car/common/overlay
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64.mk)
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64.mk)
 
 PRODUCT_NAME := aosp_car_arm64
 PRODUCT_DEVICE := generic_arm64
