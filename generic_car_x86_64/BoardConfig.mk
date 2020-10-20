2676e399a2a5d49a8d96198cfef4fda1a5bff9bb
Calvin Huang <calhuang@google.com>
2020-10-19 18:31:15 -0700

    Add 2 board configs for car emulators
    Automotive emulator images have the same device name as the phone
    emulators. Different device names help differentiating them in
    the console.
    
    1. add generic_car_x86_64 board as a wrapper of 64bit only board
    2. add generic_car_x86 board as a wrapper of generic_86
    3. change the PRODUCT_DEVICE accordingly.
    
    Test: Manually launch the emualtor, check fingerprint
    Bug: 170507246
    Change-Id: Ibaac240b112bc93ed93549909799a1491e78c844
    Merged-In: Ibaac240b112bc93ed93549909799a1491e78c844

--- generic_car_x86_64/BoardConfig.mk
+++ generic_car_x86_64/BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright (C) 2020 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,14 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-PRODUCT_PACKAGE_OVERLAYS := device/generic/car/common/overlay
+# This is a wrapper board for AAOS 64bit only emulators
+# Use generic_64bitonly_x86_64 BoardConfig as base
+include build/make/target/board/generic_64bitonly_x86_64/BoardConfig.mk
 
-$(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86_64.mk)
-
-EMULATOR_VENDOR_NO_SOUND := true
-PRODUCT_NAME := aosp_car_x86_64
-PRODUCT_DEVICE := generic_car_x86_64
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := Car on x86_64 emulator
+# Override BOARD_SUPER_PARTITION_SIZE to inclease the mounted system partition.
+BOARD_SUPER_PARTITION_SIZE := 5856296960
